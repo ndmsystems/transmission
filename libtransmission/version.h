21d9b45690a4d73f45585500e4ba280637d82209
Alexey Nikitine <nikitine@ndmsystems.ru>
2011-02-03 19:19:23 +0300

    Initial version 1.91 for backfire

---
+++ libtransmission/version.h
@@ -0,0 +1,8 @@
+#define PEERID_PREFIX             "-TR1910-"
+#define USERAGENT_PREFIX          "1.91"
+#define SVN_REVISION              "10268"
+#define SVN_REVISION_NUM          10268
+#define SHORT_VERSION_STRING      "1.91"
+#define LONG_VERSION_STRING       "1.91 (10268)"
+#define VERSION_STRING_INFOPLIST  1.91
+#define TR_STABLE_RELEASE         1
