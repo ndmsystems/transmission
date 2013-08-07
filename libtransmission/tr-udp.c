922a8d90a5b0db6ea991b238962aa8999b720839
Sergey Korolev <s.korolev@ndmsystems.com>
2013-08-07 13:52:38 +0400

    UDP receive buffer size limited to 2Mb

--- libtransmission/tr-udp.c
+++ libtransmission/tr-udp.c
@@ -42,8 +42,8 @@ THE SOFTWARE.
 /* Since we use a single UDP socket in order to implement multiple
    uTP sockets, try to set up huge buffers. */
 
-#define RECV_BUFFER_SIZE (4 * 1024 * 1024)
-#define SEND_BUFFER_SIZE (1 * 1024 * 1024)
+#define RECV_BUFFER_SIZE (2 * 1024 * 1024)
+#define SEND_BUFFER_SIZE (1 *  512 * 1024)
 #define SMALL_BUFFER_SIZE (32 * 1024)
 
 static void
